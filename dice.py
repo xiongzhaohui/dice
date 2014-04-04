387b56a9ba68ca14db87290f25d78dd9f65d0d5f
Max Xiong <xiongzhaohui1642@163.com>
2014-04-03 20:03:26 -0400

    I made it is able to ask for how many sides does his dice have

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 import random
-def in_input(object):
-    sides = int(raw_input("How many sides do you want to your dice has"))
-randomNumber = random.randint(1,6)
-print randomNumber
+def int_input(randomNumber):
+    i = int(raw_input("How many sides do you want to your dice has"))
+    randomNumber = random.randint(1,i)
+    return randomNumber
+print int_input(0)
