ca5e8412ada915fe93ca9da7eecb10ddfac1bf9c
Max Xiong <xiongzhaohui1642@163.com>
2014-04-03 20:07:30 -0400

    I add the question of asking the number of the dice.

--- dice.py
+++ dice.py
@@ -3,4 +3,5 @@ def int_input(randomNumber):
     i = int(raw_input("How many sides do you want to your dice has"))
     randomNumber = random.randint(1,i)
     return randomNumber
+number = int(raw_input("How many dice do you want to have"))
 print int_input(0)
