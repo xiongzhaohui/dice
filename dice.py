85e3f74e4b9d5973365709876208f6b994a4dd02
Max Xiong <xiongzhaohui1642@163.com>
2014-04-17 19:58:55 -0400

    finish real code

--- dice.py
+++ dice.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env
 import random
 def int_input(randomNumber):
-    i = int(raw_input("How many sides do you want to your dice has"))
-    j = int(raw_input("How many dices do you want?")
-            if i < 2:
-                return "the side of dice is wrong"
-    randomNumber = random.randint(1,i)
+    numberofdice= int(raw_input("How many dices do you want?"))
+    for i in range(1,numberofdice):
+        numberofside = int(raw_input("How many sides do you want your dice has")
+                if numberofside< 2:
+                        return "the side of dice is wrong"
+    for i in range(q,numberofdice):
+            for j in range(1,numberofside):
+                randomNumber = random.randint(1,j)
     return randomNumber
 print int_input(0)
-            #ask user how many dices
-            #ask them how many sides
+           
