edc017c5fdc5eabecaa5df1ea0b8429a25044a20
Max Xiong <xiongzhaohui1642@163.com>
2014-04-17 19:39:55 -0400

    I finish the step 5

--- dice.py
+++ dice.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env
 import random
 def int_input(randomNumber):
     i = int(raw_input("How many sides do you want to your dice has"))
     j = int(raw_input("How many dices do you want?")
+            if i < 2:
+                return "the side of dice is wrong"
     randomNumber = random.randint(1,i)
     return randomNumber
 print int_input(0)
