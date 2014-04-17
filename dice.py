18d7d792dc86dc7dd85bd3ec3abaacb26f3dc41b
Max Xiong <xiongzhaohui1642@163.com>
2014-04-17 19:20:21 -0400

    I add the pseudo code

--- dice.py
+++ dice.py
@@ -1,7 +1,9 @@
 import random
 def int_input(randomNumber):
     i = int(raw_input("How many sides do you want to your dice has"))
+    j = int(raw_input("How many dices do you want?")
     randomNumber = random.randint(1,i)
     return randomNumber
-number = int(raw_input("How many dice do you want to have"))
 print int_input(0)
+            #ask user how many dices
+            #ask them how many sides
